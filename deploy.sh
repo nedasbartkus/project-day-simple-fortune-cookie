3e01be18810a8179be16b6225c5181641b0a9c60
Lara-Ghi <lara.ghiorzo@hotmail.com>
2026-08-14 10:42:50 +0200

    CD fix not self-hosted

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash                                                                                                                
   echo "$KUBECONFIG" | base64 -d > kubeconfig
-  kubectl --kubeconfig kubeconfig apply -f k8s/ 
+  kubectl --kubeconfig kubeconfig apply --dry-run=client --validate=false -f k8s/
