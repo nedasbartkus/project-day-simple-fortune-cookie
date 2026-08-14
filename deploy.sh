d1454a19ea995bac35d9df4a19c94bb09272a636
Lara-Ghi <lara.ghiorzo@hotmail.com>
2026-08-14 10:49:17 +0200

    useless commit to trigger pipeline checks

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash                                                                                                                
   echo "$KUBECONFIG" | base64 -d > kubeconfig
   kubectl --kubeconfig kubeconfig apply --dry-run=client --validate=false -f k8s/
+  # this comment can be deleted
